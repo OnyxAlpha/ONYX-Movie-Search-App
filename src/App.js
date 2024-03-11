1b8a6944a97e8115aaf23601f2c3e5b3fa786ce3
AngeloKwakye <dangelokwakye@gmail.com>
2024-03-11 11:29:32 +0000

    routing setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from "react";
 
 import "./App.css";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
 import Homepage from "./pages/homepage/index";
 import MovieDetails from "./pages/moviedetails";
 import MovieList from "./pages/movielist";
 import PopularMovies from "./pages/popularmovies";
 import SearchResults from "./pages/searchresults";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {path:'/', element: <Homepage />},
+  {path:'/moviedetails', element: <MovieDetails />},
+  {path:'/movielists', element: <MovieList />},
+  {path:'/popularmovies', element: <PopularMovies />},
+  {path:'/searchresults', element: <SearchResults />},
+])
+
 
 
 export default function App() {
   return (
-    <>
-    <Header/>
-    <Homepage/>
-    <MovieDetails/>
-    <MovieList/>
-    <PopularMovies/>
-    <SearchResults/>
-    <Footer/>
-    </>
+    <RouterProvider router={router} />
   )
 }
--- src/pages/moviedetails/index.jsx
+++ src/pages/moviedetails/index.jsx
@@ -1,6 +1,27 @@
+import { useEffect } from "react";
 import "./index.css";
 
 export default function MovieDetails() {
+    const url = 'https://api.themoviedb.org/3/person/popular';
+   
+   
+    const getMoviesAPI =() =>{
+    fetch(url,{
+        headers: {
+            'Authorization': `Bearer ${process.env.REACT_APP_ACCESS_KEY}`
+        }
+    })
+    .then(response => response.json())
+    .then(data => console.log({data}))
+    .catch(error => console.log('Error', error))
+    }
+
+
+    useEffect(()=>{
+        getMoviesAPI();
+    },[])
+
+    
     return(
         <div>
             <p>Movie details goes here</p>
