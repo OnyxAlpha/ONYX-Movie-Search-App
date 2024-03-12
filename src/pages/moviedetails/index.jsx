a67b2dbf14003ed9911f4be8fcf812c512ef01fb
AngeloKwakye <dangelokwakye@gmail.com>
2024-03-12 11:24:56 +0000

    details

--- src/pages/moviedetails/index.jsx
+++ src/pages/moviedetails/index.jsx
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 
 export default function MovieDetails() {
+
+    const [movies, getMovies] = useState([]);
    
    
     const getMoviesAPI =() =>{
@@ -13,7 +15,10 @@ export default function MovieDetails() {
         }
     })
     .then(response => response.json())
-    .then(data => console.log({data}))
+    .then(data => {
+        console.log(data.results);
+        getMovies(data.results)
+    })
     .catch(error => console.log('Error', error))
     }
 
@@ -24,8 +29,22 @@ export default function MovieDetails() {
 
     
     return(
-        <div>
-            <p>Movie details goes here</p>
+       <>
+           <div className="container_grid">
+
+         {movies.map((item)=>{
+            return (
+             <div className="image_container">
+            <img style={{width: '80px', height: '100px'}} src={`https://image.tmdb.org/t/p/w500${item.profile_path}`} alt="" />
+            <div>
+                <p>{item.name}</p>
+            </div>
         </div>
+            )
+
+        })}
+           </div>
+        
+       </>
     )
 }
