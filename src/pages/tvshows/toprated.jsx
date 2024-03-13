b651068c7dfa2e0bcd2f9e4ce8248dc11a7c7a4c
Hannnan-Laaraba <ashraweelaaraba@gmail.com>
2024-03-13 10:24:54 +0000

    worked on popular tv shows

--- src/pages/tvshows/toprated.jsx
+++ src/pages/tvshows/toprated.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Header from '../../components/header/header.jsx';
+import Footer from '../../components/footer/footer.jsx';
 
-export default function TopRatedTVShows(){
+export default function TopRatedTVShows() {
+  const url = `https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1`;
+
+  const [topRated, setTopRated] = useState([]);
+
+  async function fetchTopRatedShows() {
+    const response = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${process.env.REACT_APP_MOVIE_API_KEY}`
+      }
+    });
+    const data = await response.json();
+    setTopRated(data.results);
+  }
+
+  useEffect(() => {
+    fetchTopRatedShows();
+  }, []);
+
+  const baseImageUrl = 'https://image.tmdb.org/t/p/w500';
+
   return (
-    <div>toprated</div>
-  )
+    <>
+      <Header />
+      <h1>Top Rated Shows</h1>
+      <div>
+        {topRated.map(show => (
+          <div key={show.id}>
+            <img src={`${baseImageUrl}${show.poster_path}`} alt='' />
+            <h2>{show.name}</h2>
+            <h3>{show.ratings}</h3>
+          </div>
+        ))}
+      </div>
+      
+      <Footer />
+    </>
+  );
 }
