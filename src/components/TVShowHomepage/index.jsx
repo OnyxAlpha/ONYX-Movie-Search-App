d8416c5dd23b498d2747ef6e5888fbc6fb75a751
Nadia-kaf <nadiabokordedzi@gmail.com>
2024-03-15 12:49:13 +0000

    final work

--- src/components/TVShowHomepage/index.jsx
+++ src/components/TVShowHomepage/index.jsx
@@ -24,26 +24,27 @@ export default function TVtrendingH() {
     
     
     return(
-      <div className="">
-      <h3>Watch Interesting TV shows <button className="bg-black  text-white rounded-lg px-5 py-2 ml-10"><Link to="/alltvshows">More</Link></button></h3>
-        <br />
-        <div  class="grid grid-cols-4 gap-4 flex overflow-x-scroll pb-10 hide-scroll-bar ">
+      <div className="drop-shadow-2xl">
+      <h3 className="pl-5">Watch Interesting TV shows <button className="bg-black  text-white rounded-lg px-5 py-1 ml-3"><Link to="/alltvshows">More</Link></button></h3>
+        <div className="w-full px-40 flex flex-col justify-center">
+        <div  class=" overflow-x-scroll pb-10 hide-scroll-bar mt-10 mb-10">
           <div className="flex flex-nowrap">
-                  {moviesData.slice(10,20).map((movie) => {
+                  {moviesData.slice(0,15).map((movie) => {
                     return (
-                      <div className="border-2 bg-white flex-shrink-0"> 
-                        <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" className="border border-black overflow-hidden max-w-xs w-30 h-40 rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"     />
-                        <br />
-                        <div className="truncate">
+                       <div>
+                         <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" className=" h-64 w-64 rounded-lg pr-1 pl-1"/>
+                        <div className="truncate pr-10 pl-10">
                         <p>{movie.name}</p>
                         <p>{movie.first_air_date}</p>
                         </div>
-                      </div>
+                       </div>
     
                     )
                   })}
                   </div>
                 </div>
+        </div>
+        
                 </div>
     )
     
--- src/components/trendingHomepage/index.jsx
+++ src/components/trendingHomepage/index.jsx
@@ -24,23 +24,23 @@ export default function TrendingH() {
     
     
     return(
-      <div>
-      <h3>Trending Movies <button className="bg-black text-white rounded-lg px-5 py-2 ml-10"><Link to="/allmovies">More</Link></button></h3>
-        <br />
-        <div  className="grid grid-cols-4 gap-4 flex overflow-x-scroll pb-10 hide-scroll-bar">
+      <div className="drop-shadow-2xl">
+      <h3 className="pl-5">Trending Movies <button className="bg-black  text-white rounded-lg px-5 py-1 ml-3"><Link to="/allmovies">More</Link></button></h3>
+        <div className="w-full px-40 flex flex-col justify-center">
+        <div  className="overflow-x-scroll pb-10 hide-scroll-bar mt-10 mb-10">
           <div className="flex flex-nowrap">
-                  {moviesData.slice(10,20).map((movie) => {
+                  {moviesData.slice(0,20).map((movie) => {
                     return (
-                      <div className="border-2 bg-white inline-block px-3"> 
-                        <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" className="border border-black overflow-hidden w-64 h-64 max-w-xs rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out"     />
-                        <br />
-                        <p className=" ">{movie.title}</p>
+                      <div> 
+                        <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" className="h-64 w-64 rounded-lg pr-1 pl-1"/>
+                        <p className="truncate pr-10 pl-10" >{movie.title}</p>
                       </div>
     
                     )
                   })}
                   </div>
                 </div>
+        </div>
                 </div>
     )
     
